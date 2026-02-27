d6f199fa9963598750c519ba2da4728f91a0f079
aimeetoledov <aimeetv11@gmail.com>
2026-02-26 19:10:52 -0600

    Document memory issues for large array in ej10.c
    Added comments explaining memory allocation issues with large arrays.

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -1,6 +1,13 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 10
+    int gigante[10000000];
+    //Si falla:
+//1. explica causa
+    // El arreglo gigante[10000000] intenta reservar aproximadamente 40 de memoria y la capacidad máxima de la pila (Stack) para cada programa es de 8 MB
+//2. indica dónde se almacena
+    //Cualquier variable declarada en main (), se guarda en automático en la pila (Stack)
+//3. propone solución sin memoria dinámica
+    //Ponerlo fuera del main (), para que se pueda declarar como una variable global.
     return 0;
 }
