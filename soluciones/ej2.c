c807a87acfb202d66c91e3e5de9ab51453280006
aimeetoledov <aimeetv11@gmail.com>
2026-02-26 18:59:03 -0600

    Implement memory address comparison for array elements
    Added code to compare memory addresses of array elements.

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,21 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 2
+
+    //Sin ejecutar determina cuál dirección es mayor:
+    //&a[4] o &a[5]
+    //la dirección de &a[5] porque la del &a[4] va siempre antes que la del &a[5] en la memoria
+    //Justifica numéricamente y luego verifica ejecutando
+
+int main() {
+    int a[5];
+
+    printf("la direccion de memoria de a[4] es: %p\n", &a[4]);
+    printf("la direccion de memoria de v[5] es: %p\n", &a[5]);
+
+    if (&a[4] < &a[5]){
+        printf("la direccion de a[5] es mayor que la de a[4]\n");
+    }
+    else if (&a[4] > &a[5]){
+        printf("la dirección de a[4] es mayor que la de a[5]\n");
+    }
     return 0;
 }
