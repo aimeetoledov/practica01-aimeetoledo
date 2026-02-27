63870dfae376aedbcabc6d53fb30cfaeda1f4ef3
aimeetoledov <aimeetv11@gmail.com>
2026-02-26 19:09:45 -0600

    Update ej9.c with array boundary explanations
    Added explanations about array boundary behavior in C.

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -1,6 +1,20 @@
-
 #include <stdio.h>
+
 int main(){
-    // Resolver ejercicio 9
+    int a[5];
+
+    printf("La direccion de a[-1] es: %p\n", &a[-1]);
+    printf("La direccion de a[5] es: %p\n", &a[5]);
+
     return 0;
 }
+
+//Explica:
+//Por qué compila
+//Porque C no verifica los límites del arreglo, por lo que, confía en el programador para no sobrepasarlos
+
+//por qué es peligroso
+//Porque está afuera del límite del arreglo y puede ocasionar resultados errróneos corrupción de memoria o fallos en nuestro programa.
+
+//Qué revela sobre memoria
+//Que es lineal, contigua y confía mucho en el programador.
