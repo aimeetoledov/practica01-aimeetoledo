18ade185fc540cc7b9b68e8e9391db5abb474a94
aimeetoledov <aimeetv11@gmail.com>
2026-02-26 19:08:28 -0600

    Add calculations and comments for 2D array size
    Added comments explaining the calculations for the number of columns and total size of a 2D array.

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,22 @@
+//Datos:
+//base = 2000
+//sizeof(int)=4
+//m[1][2] = 2024
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 8
-    return 0;
-}
+//Calcula:
+//número de columnas
+//Dirección= Base+((i*#columnas)+j)*tam_tipo 
+//Por lo tanto… 
+//2024=2000+((1*#c)+2)*4 
+//2024=2000+(#c+2)*4 
+//24=4#c+8 
+//24-8=4#c 
+//16=4#c 
+//16/4=#c 
+//4=#c 
+//Por lo tanto, m[1][2] tiene 4 columnas
+
+//tamaño total
+//Su tamaño total es de 32 bytes ya que tiene 2 filas, 4 columnas y el tamaño de un 
+//entero es de 4 bytes, si multiplicamos 2*4*2, nos da 32, por lo tanto su tamaño total es 
+//de 32 bytes.
