48036298b9c6c1b10d4aabb40fe4494d21c9aed3
aimeetoledov <aimeetv11@gmail.com>
2026-02-26 19:12:01 -0600

    Document memory address calculations for array m
    Added comments explaining the calculation of memory addresses for a 2D array.

--- soluciones/ej12.c
+++ soluciones/ej12.c
@@ -1,6 +1,18 @@
+//Se sabe:
+//int m[3][5];
+//base = 4000
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 12
-    return 0;
-}
+//Calcula:
+//1. dirección de m[2][4]
+//Tenemos la fórmula Dirección=Base+((i*#columnas)+j))*tam_tipo
+//Entonces: 
+
+//d1=4000+((2*5)+4)*4
+//d1=4056
+
+//2. dirección de m[1][3]
+//d2=4000+((1*5)+3)*4
+//d2=4032
+
+//3. diferencia entre ambas 
+//4056 - 4032 = 24 bytes
