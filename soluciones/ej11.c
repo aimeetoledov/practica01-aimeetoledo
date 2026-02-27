d6c57e7823b2fba750daf83ed0c2b8d6b3ed12b0
aimeetoledov <aimeetv11@gmail.com>
2026-02-26 19:11:26 -0600

    Add comments for exercise 11 calculations
    Added comments explaining the calculations and type size.

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -1,6 +1,16 @@
+//Salida:
+//&a[0] = 1000
+//&a[1] = 1008
+//&a[2] = 1016
 
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 11
-    return 0;
-}
+//Determina:
+//tamaño del tipo
+    //8 bytes
+//tipo posible
+    //double
+//fórmula usada
+    //Dirección=Base+ixtam_tipo
+        //1016=1000+2xtam_tipo
+          //16=2tam_tipo
+        //16/2=tam_tipo
+           //8=tam_tipo
