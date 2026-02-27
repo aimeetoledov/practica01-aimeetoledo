e8dc01d9c27837ff024935b6ad90085c1bae4d36
aimeetoledov <aimeetv11@gmail.com>
2026-02-26 19:05:02 -0600

    Update ej5.c

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -1,6 +1,4 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 5
-    return 0;
-}
+//Un estudiante afirma:
+//Si a[0] = 1000 entonces a[10] = 1010
+//Demuestra formalmente sí es correcto
+//No es correcto ya que un entero ocupa 4 bytes y no uno, por lo que si a[0]=1000 no podría ser a[10]=1010
