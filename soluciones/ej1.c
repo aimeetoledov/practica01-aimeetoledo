58e3d84d6f959a74d02ddec885d6d2cf31e484d7
aimeetoledov <aimeetv11@gmail.com>
2026-02-26 19:20:27 -0600

    Print addresses of array elements in ej1.c
    Added a loop to print the addresses of array elements.

--- soluciones/ej1.c
+++ soluciones/ej1.c
@@ -1,6 +1,16 @@
-
 #include <stdio.h>
+#define pf printf
+
 int main(){
-    // Resolver ejercicio 1
+    int a[6];
+    for(int i=0; i<6; i++){
+        pf("La direccion de a[%i] es: %p\n", i,&a[i]);
+    }
     return 0;
 }
+//1. Imprime direcciones de todos los elementos.
+//2. Calcula diferencia entre direcciones consecutivas.
+//La diferencia corresponde al espacio en la memoria de cada entero, entonces su diferencia es de 4 bytes 
+
+//3. Escribe fórmula general para dirección de a[i].
+//Base + i x tam_tipo
